5946a45b00021d91099c8209a24276f1de395aaa
delvering17 <delvering17@gmail.com>
2025-08-26 10:52:19 +0900

    Chore: Kotlin 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
 }
 
 group = "delver"
@@ -10,10 +11,18 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
--- src/main/java/delver/Main.java
+++
@@ -1,7 +0,0 @@
-package delver;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
