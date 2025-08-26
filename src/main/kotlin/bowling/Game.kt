60c35cc76a0da17cf6c1acf674345997dd6eae16
delvering17 <delvering17@gmail.com>
2025-08-26 20:51:02 +0900

    Refactor: extract method spareBonus

--- src/main/kotlin/bowling/Game.kt
+++ src/main/kotlin/bowling/Game.kt
@@ -18,7 +18,7 @@ class Game {
                 score += 10 + strikeBonus(frameIndex)
                 frameIndex += 1
             } else if (isSpare(frameIndex)) {
-                score += 10 + rolls[frameIndex + 2]
+                score += 10 + spareBonus(frameIndex)
                 frameIndex += 2
             } else {
                 score += rolls[frameIndex] + rolls[frameIndex + 1]
@@ -30,6 +30,8 @@ class Game {
     }
 
     private fun strikeBonus(frameIndex: Int) = rolls[frameIndex + 1] + rolls[frameIndex + 2]
+
+    private fun spareBonus(frameIndex: Int): Int = rolls[frameIndex + 2]
 
     private fun isStrike(frameIndex: Int) = rolls[frameIndex] == 10
 
